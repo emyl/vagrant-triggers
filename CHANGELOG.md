a70b8ab72f30fe1a94f99b83dcb1fa5e615eee70
Emiliano Ticci <emiticci@gmail.com>
2013-10-16 15:15:53 +0200

    Next version will be 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.1.1 (unreleased)
+## 0.2.0 (unreleased)
 
 IMPROVEMENTS:
 
--- lib/vagrant-triggers/version.rb
+++ lib/vagrant-triggers/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module Triggers
-    VERSION = "0.1.1.dev"
+    VERSION = "0.2.0.dev"
   end
 end
