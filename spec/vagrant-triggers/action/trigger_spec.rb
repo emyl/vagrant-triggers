9586cb4fa320aeefe0b9c2b578013b6937955101
Emiliano Ticci <emiticci@gmail.com>
2014-03-28 18:57:56 +0100

    Remove useless context

--- spec/vagrant-triggers/action/trigger_spec.rb
+++ spec/vagrant-triggers/action/trigger_spec.rb
@@ -17,51 +17,49 @@ describe VagrantPlugins::Triggers::Action::Trigger do
     machine.stub_chain(:config, :trigger, :triggers).and_return(@triggers)
   end
 
-  context "with a regular command" do
-    it "should skip :environment_load and :environment_unload actions" do
-      [:environment_load, :environment_unload].each do |action|
-        env.stub(:[]).with(:action_name).and_return(action)
-        VagrantPlugins::Triggers::DSL.should_not_receive(:new)
-        described_class.new(app, env, condition).call(env)
-      end
-    end
-
-    it "shouldn't fire if machine action is not defined" do
-      env.stub(:[]).with(:action_name)
-      env.stub(:[]).with(:machine_action).and_return(nil)
+  it "should skip :environment_load and :environment_unload actions" do
+    [:environment_load, :environment_unload].each do |action|
+      env.stub(:[]).with(:action_name).and_return(action)
       VagrantPlugins::Triggers::DSL.should_not_receive(:new)
       described_class.new(app, env, condition).call(env)
     end
+  end
 
-    it "should fire trigger when all conditions are satisfied" do
-      dsl = double("dsl")
-      VagrantPlugins::Triggers::DSL.stub(:new).with(ui, @triggers.first[:options]).and_return(dsl)
-      dsl.should_receive(:instance_eval).and_yield
-      described_class.new(app, env, condition).call(env)
-    end
+  it "shouldn't fire if machine action is not defined" do
+    env.stub(:[]).with(:action_name)
+    env.stub(:[]).with(:machine_action).and_return(nil)
+    VagrantPlugins::Triggers::DSL.should_not_receive(:new)
+    described_class.new(app, env, condition).call(env)
+  end
 
-    it "should fire all defined triggers" do
-      @triggers << @triggers.first
-      VagrantPlugins::Triggers::DSL.should_receive(:new).twice
-      described_class.new(app, env, condition).call(env)
-    end
+  it "should fire trigger when all conditions are satisfied" do
+    dsl = double("dsl")
+    VagrantPlugins::Triggers::DSL.stub(:new).with(ui, @triggers.first[:options]).and_return(dsl)
+    dsl.should_receive(:instance_eval).and_yield
+    described_class.new(app, env, condition).call(env)
+  end
 
-    it "shouldn't execute trigger with no command or block" do
-      @triggers[0][:proc] = nil
-      VagrantPlugins::Triggers::DSL.should_not_receive(:new)
-      described_class.new(app, env, condition).call(env)
-    end
+  it "should fire all defined triggers" do
+    @triggers << @triggers.first
+    VagrantPlugins::Triggers::DSL.should_receive(:new).twice
+    described_class.new(app, env, condition).call(env)
+  end
 
-    it "shouldn't fire trigger when condition doesn't match" do
-      @triggers[0][:condition] = "blah"
-      VagrantPlugins::Triggers::DSL.should_not_receive(:new)
-      described_class.new(app, env, condition).call(env)
-    end
+  it "shouldn't execute trigger with no command or block" do
+    @triggers[0][:proc] = nil
+    VagrantPlugins::Triggers::DSL.should_not_receive(:new)
+    described_class.new(app, env, condition).call(env)
+  end
 
-    it "shouldn't fire trigger when action doesn't match" do
-      @triggers[0][:action] = "blah"
-      VagrantPlugins::Triggers::DSL.should_not_receive(:new)
-      described_class.new(app, env, condition).call(env)
-    end
+  it "shouldn't fire trigger when condition doesn't match" do
+    @triggers[0][:condition] = "blah"
+    VagrantPlugins::Triggers::DSL.should_not_receive(:new)
+    described_class.new(app, env, condition).call(env)
+  end
+
+  it "shouldn't fire trigger when action doesn't match" do
+    @triggers[0][:action] = "blah"
+    VagrantPlugins::Triggers::DSL.should_not_receive(:new)
+    described_class.new(app, env, condition).call(env)
   end
 end
