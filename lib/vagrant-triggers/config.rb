97126d9fdd07b0f9604e9413030ccf079b368503
Emiliano Ticci <emiticci@gmail.com>
2014-12-28 11:07:20 +0100

    Typo

--- lib/vagrant-triggers/config.rb
+++ lib/vagrant-triggers/config.rb
@@ -3,7 +3,7 @@ module VagrantPlugins
     module Config
       # Autoload farm
       config_root = Pathname.new(File.expand_path("../config", __FILE__))
-      autoload :Provisioner, config_root.join("Provisioner")
+      autoload :Provisioner, config_root.join("provisioner")
       autoload :Trigger,     config_root.join("trigger")
     end
   end
